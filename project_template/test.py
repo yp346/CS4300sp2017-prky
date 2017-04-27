4ee3e7192b023e3962da5b9776b9d259a9387360
Ria Mirchandani <Ria@dhcp-rhodes-2331.eduroam.cornell.edu>
2017-04-27 17:49:09 -0400

    tfidf_sim_senti_weighted

--- project_template/test.py
+++ project_template/test.py
@@ -128,6 +128,84 @@ def tfidf_sentiment_sim(q):
 
     return final_return_list
 
+def tfidf_sentiment_sim_weighted(q):
+    dir_cur = os.path.dirname(__file__)
+    file_path_obj = os.path.join(dir_cur,"../jsons/tfidf_obj.pk")
+    file_path_np = os.path.join(dir_cur,"../jsons/tf_idf.npy")
+    file_features = os.path.join(dir_cur,"../jsons/feature_names.json")
+    course_desc_path = os.path.join(dir_cur,"../jsons/course_desc_list.json")
+    course_url_path = os.path.join(dir_cur,"../jsons/course_url_list.json")
+    course_name_path = os.path.join(dir_cur,"../jsons/course_name_list.json")
+    coursera_senti_score_path = os.path.join(dir_cur,"../jsons/senti_score_dict.json")
+    
+    with open(coursera_senti_score_path,"r") as fp:
+        content = fp.read()
+        coursera_senti_dict = json.loads(content.decode("utf-8","ignore"))
+    
+    with open(file_path_obj,"rb") as fp:
+        vectorizer = pickle.load(fp)
+    with open(file_path_np,"rb") as fp:
+        doc_by_vocab = np.load(fp)
+    with open(file_features,"r") as fp:
+        content = fp.read()
+        features = json.loads(content)#.decode("utf-8","ignore"))
+    with open(course_desc_path,"r") as fp:
+        content = fp.read()
+    desc_list = json.loads(content.decode("utf-8","ignore"))
+    with open(course_url_path,"r") as fp:
+        content = fp.read()
+    url_list = json.loads(content.decode("utf-8","ignore"))
+    with open(course_name_path,"r") as fp:
+        content = fp.read()
+    name_list = json.loads(content.decode("utf-8","ignore"))
+    new_tfidf =  TfidfVectorizer(vocabulary=features,max_features=5000, stop_words="english",norm="l2")
+    query_list = q.split(",")
+    query_by_vocab = new_tfidf.fit_transform(query_list).toarray()
+    sim_matrix = np.dot(query_by_vocab,doc_by_vocab.T)
+    #sim_matrix = final.flatten()
+    #sorted_sim = np.argsort(-sim_matrix)
+    #sorted_values = np.sort(sim_matrix)
+    #sorted_values = np.fliplr(sorted_values)
+
+    #return_list = []
+    #for x in sorted_sim:
+    #    temp = []
+    #    for y in x[:5]:
+    #        temp.append((name_list[y],url_list[y]))
+    #    return_list.append(temp)
+
+    senti_score_list = []
+    for skill in query_list:
+        temp_senti = []
+        for url in url_list:
+        	if coursera_senti_dict[url]:
+            		temp_senti.append(coursera_senti_dict[url])
+            	else:
+            		temp_senti.append(0.0)
+        senti_score_list.append(temp_senti)
+        
+    senti_score_matrix = np.array(senti_score_list)
+    
+    final_score_matrix = senti_score_matrix + sim_matrix
+    
+    sorted_final_score = np.argsort(-final_score_matrix)
+    
+    return_list = []
+    for i in range(sorted_final_score.shape[0]):
+        temp = []
+        for j in sorted_final_score[i][:5]:
+            temp.append((str(final_score_matrix[i][j]),name_list[j],url_list[j]))
+        return_list.append(temp)
+
+    #final_return_list = []
+    #for skill in senti_score_list:
+    #    skill.sort(reverse = True)
+    #    temp = []
+    #    for t in skill[:5]:
+    #        temp.append((str(t[0]),t[1],t[2]))
+    #    final_return_list.append(temp)
+
+    return return_list
 
 
 
--- project_template/views.py
+++ project_template/views.py
@@ -7,6 +7,7 @@ from .form import QueryForm
 from .test import find_similar
 from .test import tfidf_sim
 from .test import tfidf_sentiment_sim
+from .test import tfidf_sentiment_sim_weighted
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 import nltk
 
@@ -23,16 +24,16 @@ def index(request):
     #version = request.GET.get('submit_button')
     if request.GET.get('known_courses'):
         known_courses1 = request.GET.getlist('known_courses')
-	known_courses = ",".join(known_courses1)
+        known_courses = ",".join(known_courses1)
     if request.GET.get('search'):
     	original_search = request.GET.get('search')
-	if version == 'First':
-        	search = request.GET.get('search')
-        	output_list = find_similar(search)
-        	output = tfidf_sim(search)
-        	search = search.split(",")
-        	zipped = zip(search, output)
-        else:
+    	if version == 'First':
+    		search = request.GET.get('search')
+    		output_list = find_similar(search)
+    		output = tfidf_sim(search)
+    		search = search.split(",")
+    		zipped = zip(search, output)
+        elif version == 'Second':
         	patterns = """
         				NP: {<NNP>+}
     					{<DT|PP\$>?<JJ>*<NN*>+}
@@ -51,15 +52,43 @@ def index(request):
         	#search = ",".join(nps)
         	#output_list = find_similar(search)
         	if known_courses!="":
-		    output = tfidf_sentiment_sim(known_courses)
-		    zipped = zip(known_courses1,output)
-		    second_select = "False"
+		    	output = tfidf_sentiment_sim(known_courses)
+		    	zipped = zip(known_courses1,output)
+		    	second_select = "False"
 		else:
-		    output = "s"*len(nps)
-        	#search = search.split(",")
-        	    #zipped = zip(search, output)
-		    zipped = zip(nps,output)
-		    second_select = "True"
+		    	output = "s"*len(nps)
+        		#search = search.split(",")
+        	    	#zipped = zip(search, output)
+        	    	zipped = zip(nps,output)
+			second_select = "True"
+	else:
+        	patterns = """
+        				NP: {<NNP>+}
+    					{<DT|PP\$>?<JJ>*<NN*>+}
+    					"""
+    		NPChunker = nltk.RegexpParser(patterns)
+        	search = request.GET.get('search')
+        	words = nltk.word_tokenize(search)
+        	tags = nltk.pos_tag(words)
+        	tree = NPChunker.parse(tags)
+    		nps = []
+    		for subtree in tree.subtrees():
+    			if subtree.label() == 'NP':
+    				t = subtree
+    				t = ' '.join(word for word, tag in t.leaves())
+    				nps.append(t)
+        	#search = ",".join(nps)
+        	#output_list = find_similar(search)
+        	if known_courses!="":
+		    	output = tfidf_sentiment_sim_weighted(known_courses)
+		    	zipped = zip(known_courses1,output)
+		    	second_select = "False"
+		else:
+		    	output = "s"*len(nps)
+        		#search = search.split(",")
+        	    	#zipped = zip(search, output)
+        	    	zipped = zip(nps,output)
+			second_select = "True"
         #paginator = Paginator(output_list1, 4)
         #page = request.GET.get('page')
         #try:
